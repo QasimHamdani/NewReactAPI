c0860243e522ea04781d2303bc13b2dd8184c202
qasimhamdani <qasim_hamdani26@milton.edu>
2025-12-15 19:45:30 -0500

    did stuff

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,61 +1,40 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Colours from "./Colour";
-import axios from 'axios';
+import axios from "axios";
 
-const [Data, setData]=useState({
-    compamy:'',
-    Description:''
+const Home = () => {
+  const [data, setData] = useState({
+    company: "",
+    description: "",
+  });
 
-})
-const [coloursData, setColoursData]=useState([])
+  const [coloursData, setColoursData] = useState([]);
 
-const Home=()=>{
+  useEffect(() => {
+    axios
+      .get("https://collectionapi.metmuseum.org/public/collection/v1/objects")
+      
+        .then((res) => {
+           const companyName = "rainbow books";
+           const companyDesc = "We connect readers to their new fav queer reads";
 
-    useEffect(()=>{
-        axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects' 
+           setData({ company: companyName, description: companyDesc });
+
         
-)
-           
-{
-	"total": 471581,
-	"objectIDs": [
-		1,
-		2,
-		3,
-		4,
-		5,
-		6,
-		7,
-		8,
-		9,
-		10,
-		// more results ...
-	]
-}
+           setColoursData(res.data.objectIDs); 
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
+  return (
+    <>
+      <h1>{data.company}</h1>
+      <p>{data.description}</p>
+      <Colours data={coloursData} />
+    </>
+  );
+};
 
-
-
-
-.then(res=>{
-                let companyName = "rainbow books";
-                let companyDesc = "We connect readers to their new fav queer reads";
-
-                setData({Company:companyName, Description:companyDesc})
-                setColoursData(res.data.data)
-            })
-            
-            
-            .catch(err=>{
-                console.log(err);
-            })
-    },[])
-    return(
-        <>
-            <h1>{Data.Company}</h1> 
-            <p>{Data.Description}</p>
-            <Colours data ={coloursData}/>
-        </>
-    )
-}
 export default Home;
