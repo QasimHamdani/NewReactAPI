b854e392845e8580a3f4b930ea3a1f3b7b1c3ca8
qasimhamdani <qasim_hamdani26@milton.edu>
2026-01-08 08:39:58 -0500

    did stuff

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,34 @@ import ReactDOM from 'react-dom/client';
 import './App.css'
 import Home from './Home'
 import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Login from "./Login.jsx";
+
+const Button = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      style={{
+        padding: "10px 20px",
+        fontSize: "16px",
+        cursor: "pointer",
+        outline: "none",
+        border: "2px solid green",
+        borderRadius: "4px",
+        marginTop: "16px",
+        marginBottom: "16px",
+      }}
+    >
+      Go to Home
+    </button>
+  );
+};
 
 function App() {
 
@@ -19,5 +47,9 @@ root.render(
     <App />
   </BrowserRouter>
 );
+
+<Routes>
+      <Route path="/login" element={<Login />} />
+</Routes>
 
 export default App
